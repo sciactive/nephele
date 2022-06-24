ceb18f1faa2ca752c8e7039d377874188ddeb0e7
Hunter Perrin <hperrin@gmail.com>
2022-06-24 16:26:05 -0700

    refactor: move interfaces into subfolder (2)

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import type {
+  Adapter,
+  Cache,
+  Lock,
+  Properties,
+  Resource,
+  User,
+} from './Interfaces/index.js';
+import type { Options } from './Options.js';
+
+export * from './Errors/index.js';
+import createServer from './createServer.js';
+import { defaults } from './Options.js';
+
+export { Adapter, Cache, Lock, Properties, Resource, User, Options };
+
+export { createServer, defaults };
+
+export default createServer;
