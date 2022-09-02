cd7757955cd66c0a93c9b47e689eddbdc3772d09
Hunter Perrin <hperrin@gmail.com>
2022-09-02 12:59:03 -0700

    fix: load server from cjs file for compatibility with pm2

---
+++ packages/nephele-serve/nephele-serve.cjs
@@ -0,0 +1,3 @@
+#!/usr/bin/env -S NODE_OPTIONS="--experimental-specifier-resolution=node ${NODE_OPTIONS}" node
+
+import('./bin/index.js');
--- packages/nephele-serve/nephele-serve.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env -S NODE_OPTIONS='--experimental-specifier-resolution=node' node
-
-import './bin/index.js';
