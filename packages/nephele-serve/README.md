aac721836b536c2dbaee911cb92f066e57d8fc6a
Hunter Perrin <hperrin@gmail.com>
2022-09-02 10:40:14 -0700

    feat: new nephele-serve package to run nephele and serve local files

---
+++ packages/nephele-serve/README.md
@@ -0,0 +1,117 @@
+# Nephele - Local File Server
+
+Run Nephele WebDAV server to serve local files.
+
+# Installation
+
+```sh
+sudo npm install -g nephele-serve
+```
+
+# Usage
+
+To serve the current directory.
+
+```sh
+sudo nephele-serve .
+```
+
+To serve users' home directories.
+
+```sh
+sudo nephele-serve --home-directories
+```
+
+To serve user directories under the current directory. This creates directories within the server root with the users' usernames, and serves their own directory to them.
+
+```sh
+sudo nephele-serve --user-directories .
+```
+
+If you want to run it without installing it, you can do that too.
+
+```sh
+sudo npx nephele-serve .
+```
+
+If you want to run it without root, you can do that too, but you must set the port to something higher than 1000.
+
+```sh
+nephele-serve -p 8080 .
+```
+
+Only regular users (UIDs 1000-59999) are allowed to log in.
+
+# Help
+
+```sh
+nephele-serve --help
+```
+
+Here's a copy of the help output:
+
+```
+Usage: nephele-serve [options] [directory]
+
+Run Nephele WebDAV server to serve local files.
+
+Arguments:
+  directory            The path of the directory to use as the server root.
+
+Options:
+  -v, --version        Print the current version
+  -h, --host <host>    A host address to listen on. The default is to listen on all hosts. (default: "::")
+  -r, --realm <realm>  The realm reported to the user by the server when authentication is requested. Defaults to the system hostname.
+  --cert <cert_file>   The filename of a certificate to use for HTTPS in PEM format.
+  --key <key_file>     The filename of a private key to use for HTTPS in PEM format.
+  -p, --port <port>    The port to listen on. Defaults to 443 if a cert is provided, 80 otherwise.
+  --home-directories   Serve users' home directories to them when they log in.
+  --user-directories   Serve users their own directory under the server root when they log in.
+  --no-auth            Don't require authorization. (Not compatible with serving home directories or user directories.)
+  --help               display help for command
+
+Environment Variables:
+  HOST                 Same as --host.
+  REALM                Same as --realm.
+  CERT_FILE            Same as --cert.
+  CERT                 Text of a cert in PEM format.
+  KEY_FILE             Same as --key.
+  KEY                  Text of a key in PEM format.
+  HOME_DIRECTORIES     Same as --home-directories when set to "true", "on" or "1".
+  USER_DIRECTORIES     Same as --user-directories when set to "true", "on" or "1".
+  AUTH                 Same as --no-auth when set to "false", "off" or "0".
+  SERVER_ROOT          Same as [directory].
+
+Options given on the command line take precedence over options from an environment variable.
+
+Nephele repo: https://github.com/sciactive/nephele
+Copyright (C) 2022 SciActive, Inc
+https://sciactive.com/
+```
+
+# Node Warning
+
+If you see this warning:
+
+```
+(node:319632) ExperimentalWarning: The Node.js specifier resolution flag is experimental. It could change or be removed at any time.
+(Use `node --trace-warnings ...` to show where the warning was created)
+```
+
+Don't worry, it's because the script is using `--experimental-specifier-resolution=node` to be able to load the PAM authentication module from an ES module.
+
+# License
+
+Copyright 2022 SciActive Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ packages/nephele-serve/src/index.ts
@@ -0,0 +1,324 @@
+import type { Server } from 'node:http';
+import https from 'node:https';
+import http from 'node:http';
+import { networkInterfaces, hostname } from 'node:os';
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { program, Option } from 'commander';
+import { homedir as userHomePath } from 'userhomepath';
+import userid from 'userid';
+import express from 'express';
+import nepheleServer from 'nephele';
+import FileSystemAdapter from '@nephele/adapter-file-system';
+import VirtualAdapter from '@nephele/adapter-virtual';
+import PamAuthenticator from '@nephele/authenticator-pam';
+import InsecureAuthenticator from '@nephele/authenticator-none';
+
+type Hosts = {
+  name: string;
+  family: string;
+  address: string;
+}[];
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pkg = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '..', 'package.json')).toString()
+);
+
+const { ids } = userid;
+
+type Conf = {
+  host: string;
+  realm?: string;
+  cert?: string;
+  key?: string;
+  port?: number;
+  homeDirectories: boolean;
+  userDirectories: boolean;
+  auth: boolean;
+  directory?: string;
+};
+
+program
+  .name(pkg.name)
+  .description(pkg.description)
+  .version(pkg.version, '-v, --version', 'Print the current version');
+
+program
+  .option(
+    '-h, --host <host>',
+    'A host address to listen on. The default is to listen on all hosts.',
+    '::'
+  )
+  .option(
+    '-r, --realm <realm>',
+    'The realm reported to the user by the server when authentication is requested. Defaults to the system hostname.'
+  )
+  .option(
+    '--cert <cert_file>',
+    'The filename of a certificate to use for HTTPS in PEM format.'
+  )
+  .option(
+    '--key <key_file>',
+    'The filename of a private key to use for HTTPS in PEM format.'
+  )
+  .addOption(
+    new Option(
+      '-p, --port <port>',
+      'The port to listen on. Defaults to 443 if a cert is provided, 80 otherwise.'
+    ).argParser(parseInt)
+  )
+  .option(
+    '--home-directories',
+    "Serve users' home directories to them when they log in."
+  )
+  .option(
+    '--user-directories',
+    'Serve users their own directory under the server root when they log in.'
+  )
+  .option(
+    '--no-auth',
+    "Don't require authorization. (Not compatible with serving home directories or user directories.)"
+  )
+  .argument(
+    '[directory]',
+    'The path of the directory to use as the server root.'
+  );
+
+program.addHelpText(
+  'after',
+  `
+Environment Variables:
+  HOST                 Same as --host.
+  REALM                Same as --realm.
+  CERT_FILE            Same as --cert.
+  CERT                 Text of a cert in PEM format.
+  KEY_FILE             Same as --key.
+  KEY                  Text of a key in PEM format.
+  HOME_DIRECTORIES     Same as --home-directories when set to "true", "on" or "1".
+  USER_DIRECTORIES     Same as --user-directories when set to "true", "on" or "1".
+  AUTH                 Same as --no-auth when set to "false", "off" or "0".
+  SERVER_ROOT          Same as [directory].
+
+Options given on the command line take precedence over options from an environment variable.`
+);
+
+program.addHelpText(
+  'afterAll',
+  `
+Nephele repo: https://github.com/sciactive/nephele
+Copyright (C) 2022 SciActive, Inc
+https://sciactive.com/`
+);
+
+try {
+  // Parse args.
+  program.parse();
+  const options = program.opts();
+  let {
+    host,
+    realm,
+    cert,
+    key,
+    port,
+    homeDirectories,
+    userDirectories,
+    auth,
+    directory,
+  } = {
+    host: process.env.HOST,
+    realm: process.env.REALM || hostname(),
+    cert: process.env.CERT_FILE,
+    key: process.env.KEY_FILE,
+    homeDirectories: ['true', 'on', '1'].includes(
+      (process.env.HOME_DIRECTORIES || '').toLowerCase()
+    ),
+    userDirectories: ['true', 'on', '1'].includes(
+      (process.env.USER_DIRECTORIES || '').toLowerCase()
+    ),
+    auth: !['false', 'off', '0'].includes(
+      (process.env.AUTH || '').toLowerCase()
+    ),
+    directory: program.args.length
+      ? path.resolve(program.args[0])
+      : process.env.SERVER_ROOT && path.resolve(process.env.SERVER_ROOT),
+    ...options,
+  } as Conf;
+
+  if (cert) {
+    cert = fs.readFileSync(path.resolve(cert)).toString();
+  } else {
+    cert = process.env.CERT;
+  }
+
+  if (key) {
+    key = fs.readFileSync(path.resolve(key)).toString();
+  } else {
+    key = process.env.KEY;
+  }
+
+  const secure = !!(cert && key);
+  if (port == null) {
+    port = parseInt(process.env.PORT || (secure ? '443' : '80'));
+  }
+
+  // Validate args.
+  if (homeDirectories && userDirectories) {
+    throw new Error(
+      'Only one of --home-directories and --user-directories options can be used at a time.'
+    );
+  }
+
+  if (directory != null && homeDirectories) {
+    throw new Error("Can't serve both a directory and home directories.");
+  }
+
+  if (userDirectories && directory == null) {
+    throw new Error('Serving user directories requires a root directory.');
+  }
+
+  if (directory == null && !homeDirectories) {
+    throw new Error(
+      'A root directory or the --home-directories option is required.'
+    );
+  }
+
+  if ((homeDirectories || userDirectories) && !auth) {
+    throw new Error(
+      'The --home-directories and --user-directories options require authentication.'
+    );
+  }
+
+  if (directory != null && !fs.statSync(directory).isDirectory()) {
+    throw new Error('Provided server root is not an accessible directory.');
+  }
+
+  // Get server ready.
+  const getHosts = () => {
+    const ifaces = networkInterfaces();
+    let serverHosts: Hosts = [];
+    for (let name in ifaces) {
+      const netDict = ifaces[name];
+      if (netDict == null) {
+        continue;
+      }
+      for (let net of netDict) {
+        const family =
+          typeof net.family === 'string' ? net.family : `IPv${net.family}`;
+        if (!net.internal && net.address) {
+          if (host.trim() !== '::' && host.trim() !== net.address) {
+            continue;
+          }
+          serverHosts.push({ name, family, address: net.address });
+        }
+      }
+    }
+    return serverHosts;
+  };
+
+  const serverHosts = getHosts();
+  const app = express();
+  const instanceDate = new Date();
+
+  if (serverHosts.length === 0) {
+    throw new Error('No hosts to listen on.');
+  }
+
+  app.use(
+    '/',
+    nepheleServer({
+      adapter: async (_request, response) => {
+        if (auth && response.locals.user == null) {
+          return new VirtualAdapter({
+            files: {
+              properties: {
+                creationdate: instanceDate,
+                getlastmodified: instanceDate,
+                owner: 'root',
+              },
+              locks: {},
+              children: [],
+            },
+          });
+        }
+
+        try {
+          if (homeDirectories) {
+            const root = await userHomePath(response.locals.user.username);
+            return new FileSystemAdapter({
+              root,
+              usernamesMapToSystemUsers: true,
+            });
+          }
+
+          if (directory == null || !fs.statSync(directory).isDirectory()) {
+            throw new Error('Server root is not an accessible directory.');
+          }
+
+          if (userDirectories) {
+            const root = path.join(
+              directory,
+              response.locals.user.username.replace(/\//g, '_')
+            );
+
+            try {
+              fs.accessSync(root);
+            } catch (e: any) {
+              fs.mkdirSync(root);
+              const { uid, gid } = ids(response.locals.user.username);
+              fs.chownSync(root, uid, gid);
+            }
+
+            return new FileSystemAdapter({
+              root,
+              usernamesMapToSystemUsers: true,
+            });
+          }
+
+          return new FileSystemAdapter({
+            root: directory,
+            usernamesMapToSystemUsers: true,
+          });
+        } catch (e) {
+          throw new Error("Couldn't mount server root.");
+        }
+      },
+      authenticator: auth
+        ? new PamAuthenticator({ realm })
+        : new InsecureAuthenticator(),
+    })
+  );
+
+  // Run server.
+  let server: Server;
+  if (secure) {
+    server = https
+      .createServer({ cert, key }, app)
+      .listen(port, host === '::' ? undefined : host);
+  } else {
+    server = http
+      .createServer({}, app)
+      .listen(port, host === '::' ? undefined : host);
+  }
+
+  server.on('listening', () => {
+    console.log(
+      `Nephele server listening on ${serverHosts
+        .map(
+          ({ name, address }) =>
+            `dav${secure ? 's' : ''}://${address}:${port} (${name})`
+        )
+        .join(', ')}`
+    );
+  });
+
+  server.on('close', () => {
+    console.log('Nephele server closed.');
+    process.exit(0);
+  });
+} catch (e: any) {
+  console.error('Error:', e.message);
+  process.exit(1);
+}
