98bcc65cc4d0c2838865cae5d79a2156bc0c1602
Hunter Perrin <hperrin@gmail.com>
2022-11-09 13:39:13 -0800

    feat: use the same refresh button for all request in index plugin

--- packages/plugin-index/src/IndexPage.svelte
+++ packages/plugin-index/src/IndexPage.svelte
@@ -1,273 +1,275 @@
 <svelte:head>
   <script>
-    /**
-     * Make Directory
-     */
     document.addEventListener('DOMContentLoaded', () => {
-      const mkdirContainer = document.getElementById('mkdirContainer');
-      mkdirContainer.style.display = '';
-      const mkdirForm = document.getElementById('mkdir');
-      const mkdirOutput = document.getElementById('mkdirOutput');
-      const mkdirs = document.getElementById('mkdirs');
+      const refreshContainer = document.getElementById('refreshContainer');
       const refresh = document.getElementById('refresh');
       let requests = [];
 
-      mkdirForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        requests = requests.concat([
-          {
-            done: null,
-            name: mkdirForm.name.value,
-          },
-        ]);
-        mkdirForm.reset();
+      /**
+       * Make Directory
+       */
+      handleMkdirs: {
+        const mkdirContainer = document.getElementById('mkdirContainer');
+        mkdirContainer.style.display = '';
+        const mkdirForm = document.getElementById('mkdir');
+        const mkdirs = document.getElementById('mkdirs');
 
-        doMkdir();
-      });
+        mkdirForm.addEventListener('submit', (event) => {
+          event.preventDefault();
+          requests = requests.concat([
+            {
+              type: 'mkdir',
+              done: null,
+              name: mkdirForm.name.value,
+            },
+          ]);
+          mkdirForm.reset();
 
-      function doMkdir() {
-        for (let i = 0; i < requests.length; i++) {
-          const dir = requests[i];
+          doMkdir();
+        });
 
-          if (dir.done === null) {
-            dir.done = false;
-            mkdirOutput.style.display = '';
-            refresh.disabled = true;
+        function doMkdir() {
+          for (let i = 0; i < requests.length; i++) {
+            const dir = requests[i];
 
-            const element = document.createElement('div');
-            element.style.marginBottom = '0.5em';
+            if (dir.type === 'mkdir' && dir.done === null) {
+              dir.done = false;
+              mkdirs.style.display = '';
+              refreshContainer.style.display = '';
+              refresh.disabled = true;
 
-            const progress = document.createElement('div');
-            progress.style.display = 'inline-block';
-            progress.innerText = 'Working...';
-            progress.style.border = '1px solid #000';
-            progress.style.width = '100px';
-            progress.style.marginRight = '0.5em';
-            progress.style.textAlign = 'center';
-            element.appendChild(progress);
+              const element = document.createElement('div');
+              element.style.marginBottom = '0.5em';
 
-            const name = document.createElement('span');
-            name.innerText = dir.name;
-            element.appendChild(name);
+              const progress = document.createElement('div');
+              progress.style.display = 'inline-block';
+              progress.innerText = 'Working...';
+              progress.style.border = '1px solid #000';
+              progress.style.width = '100px';
+              progress.style.marginRight = '0.5em';
+              progress.style.textAlign = 'center';
+              element.appendChild(progress);
 
-            mkdirs.appendChild(element);
+              const name = document.createElement('span');
+              name.innerText = dir.name;
+              element.appendChild(name);
 
-            const xhr = new XMLHttpRequest();
-            xhr.addEventListener('loadend', () => {
-              if (
-                xhr.readyState === 4 &&
-                xhr.status >= 200 &&
-                xhr.status < 300
-              ) {
-                progress.innerText = 'Success.';
-              } else {
-                progress.innerText = `Error: ${xhr.status} ${xhr.statusText}`;
-              }
-              dir.done = true;
+              mkdirs.appendChild(element);
 
-              if (requests.find((dir) => !dir.done) == null) {
-                refresh.disabled = false;
-              }
-            });
-            xhr.open('MKCOL', dir.name, true);
-            xhr.send();
+              const xhr = new XMLHttpRequest();
+              xhr.addEventListener('loadend', () => {
+                if (
+                  xhr.readyState === 4 &&
+                  xhr.status >= 200 &&
+                  xhr.status < 300
+                ) {
+                  progress.innerText = 'Success.';
+                } else {
+                  progress.innerText = `Error: ${xhr.status} ${xhr.statusText}`;
+                }
+                dir.done = true;
+
+                if (requests.find((request) => !request.done) == null) {
+                  refresh.disabled = false;
+                }
+              });
+              xhr.open('MKCOL', dir.name, true);
+              xhr.send();
+            }
           }
         }
       }
-    });
 
-    /**
-     * File Upload
-     */
-    document.addEventListener('DOMContentLoaded', () => {
-      const uploadContainer = document.getElementById('uploadContainer');
-      uploadContainer.style.display = '';
-      const uploadForm = document.getElementById('upload');
-      const uploadOutput = document.getElementById('uploadOutput');
-      const uploads = document.getElementById('uploads');
-      const refresh = document.getElementById('refresh');
-      let requests = [];
+      /**
+       * File Upload
+       */
+      handleUploads: {
+        const uploadContainer = document.getElementById('uploadContainer');
+        uploadContainer.style.display = '';
+        const uploadForm = document.getElementById('upload');
+        const uploads = document.getElementById('uploads');
 
-      uploadForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        requests = requests.concat(
-          Array.prototype.slice.call(uploadForm.file.files).map((file) => ({
-            done: null,
-            file,
-          }))
-        );
-        uploadForm.reset();
+        uploadForm.addEventListener('submit', (event) => {
+          event.preventDefault();
+          requests = requests.concat(
+            Array.prototype.slice.call(uploadForm.file.files).map((file) => ({
+              type: 'upload',
+              done: null,
+              file,
+            }))
+          );
+          uploadForm.reset();
 
-        doUpload();
-      });
+          doUpload();
+        });
 
-      function doUpload() {
-        for (let i = 0; i < requests.length; i++) {
-          const file = requests[i];
+        function doUpload() {
+          for (let i = 0; i < requests.length; i++) {
+            const file = requests[i];
 
-          if (file.done === null) {
-            file.done = false;
-            uploadOutput.style.display = '';
-            refresh.disabled = true;
+            if (file.type === 'upload' && file.done === null) {
+              file.done = false;
+              uploads.style.display = '';
+              refreshContainer.style.display = '';
+              refresh.disabled = true;
 
-            const element = document.createElement('label');
-            element.style.display = 'block';
-            element.style.marginBottom = '0.5em';
+              const element = document.createElement('label');
+              element.style.display = 'block';
+              element.style.marginBottom = '0.5em';
 
-            const progress = document.createElement('progress');
-            progress.value = 0;
-            progress.max = 100;
-            progress.style.width = '100px';
-            progress.style.marginRight = '0.5em';
-            element.appendChild(progress);
+              const progress = document.createElement('progress');
+              progress.value = 0;
+              progress.max = 100;
+              progress.style.width = '100px';
+              progress.style.marginRight = '0.5em';
+              element.appendChild(progress);
 
-            const name = document.createElement('span');
-            name.innerText = file.file.name;
-            element.appendChild(name);
+              const name = document.createElement('span');
+              name.innerText = file.file.name;
+              element.appendChild(name);
 
-            uploads.appendChild(element);
+              uploads.appendChild(element);
 
-            const xhr = new XMLHttpRequest();
-            xhr.upload.addEventListener('progress', (event) => {
-              if (event.lengthComputable) {
-                progress.value = (event.loaded / event.total) * 100;
-                progress.title = (event.loaded / event.total) * 100 + '%';
-              }
-            });
-            xhr.addEventListener('progress', (event) => {
-              if (event.lengthComputable) {
-                progress.value = (event.loaded / event.total) * 100;
-                progress.title = (event.loaded / event.total) * 100 + '%';
-              }
-            });
-            xhr.addEventListener('loadend', () => {
-              if (
-                xhr.readyState === 4 &&
-                xhr.status >= 200 &&
-                xhr.status < 300
-              ) {
-                progress.value = 100;
-                progress.title = 'Done';
-              } else {
-                progress.value = 0;
-                progress.title = 'Error';
-                const error = document.createElement('span');
-                error.innerText = ` (Error: ${xhr.status} ${xhr.statusText})`;
-                element.appendChild(error);
-              }
-              file.done = true;
+              const xhr = new XMLHttpRequest();
+              xhr.upload.addEventListener('progress', (event) => {
+                if (event.lengthComputable) {
+                  progress.value = (event.loaded / event.total) * 100;
+                  progress.title = (event.loaded / event.total) * 100 + '%';
+                }
+              });
+              xhr.addEventListener('progress', (event) => {
+                if (event.lengthComputable) {
+                  progress.value = (event.loaded / event.total) * 100;
+                  progress.title = (event.loaded / event.total) * 100 + '%';
+                }
+              });
+              xhr.addEventListener('loadend', () => {
+                if (
+                  xhr.readyState === 4 &&
+                  xhr.status >= 200 &&
+                  xhr.status < 300
+                ) {
+                  progress.value = 100;
+                  progress.title = 'Done';
+                } else {
+                  progress.value = 0;
+                  progress.title = 'Error';
+                  const error = document.createElement('span');
+                  error.innerText = ` (Error: ${xhr.status} ${xhr.statusText})`;
+                  element.appendChild(error);
+                }
+                file.done = true;
 
-              if (requests.find((file) => !file.done) == null) {
-                refresh.disabled = false;
-              }
-            });
-            xhr.open('PUT', file.file.name, true);
-            xhr.setRequestHeader('Content-Type', file.file.type);
-            xhr.send(file.file);
+                if (requests.find((request) => !request.done) == null) {
+                  refresh.disabled = false;
+                }
+              });
+              xhr.open('PUT', file.file.name, true);
+              xhr.setRequestHeader('Content-Type', file.file.type);
+              xhr.send(file.file);
+            }
           }
         }
       }
-    });
 
-    /**
-     * Show Actions
-     */
-    document.addEventListener('DOMContentLoaded', () => {
+      /**
+       * Show Actions
+       */
       const actions = document.querySelectorAll('.action');
 
       for (let i = 0; i < actions.length; i++) {
         const action = actions[i];
         action.style.display = '';
       }
-    });
 
-    /**
-     * Delete
-     */
-    document.addEventListener('DOMContentLoaded', () => {
-      const fileTable = document.getElementById('fileTable');
-      const deleteOutput = document.getElementById('deleteOutput');
-      const deletes = document.getElementById('deletes');
-      const refresh = document.getElementById('refresh');
-      let requests = [];
+      /**
+       * Delete
+       */
+      handleDeletes: {
+        const fileTable = document.getElementById('fileTable');
+        const deleteContainer = document.getElementById('deleteContainer');
+        const deletes = document.getElementById('deletes');
 
-      fileTable.addEventListener('click', (event) => {
-        if (!event.target.classList.contains('delete')) {
-          return;
-        }
+        fileTable.addEventListener('click', (event) => {
+          if (!event.target.classList.contains('delete')) {
+            return;
+          }
 
-        event.preventDefault();
+          event.preventDefault();
 
-        if (
-          !confirm(
-            `Are you sure you want to delete "${
-              event.target.parentNode.dataset['name']
-            }"?${
-              event.target.parentNode.dataset['name'].match(/\/$/)
-                ? ' This will delete all of its contents too.'
-                : ''
-            }`
-          )
-        ) {
-          return;
-        }
+          if (
+            !confirm(
+              `Are you sure you want to delete "${
+                event.target.parentNode.dataset['name']
+              }"?${
+                event.target.parentNode.dataset['name'].match(/\/$/)
+                  ? ' This will delete all of its contents too.'
+                  : ''
+              }`
+            )
+          ) {
+            return;
+          }
 
-        requests = requests.concat([
-          {
-            done: null,
-            name: event.target.parentNode.dataset['name'],
-          },
-        ]);
+          requests = requests.concat([
+            {
+              type: 'delete',
+              done: null,
+              name: event.target.parentNode.dataset['name'],
+            },
+          ]);
 
-        doDelete();
-      });
+          doDelete();
+        });
 
-      function doDelete() {
-        for (let i = 0; i < requests.length; i++) {
-          const file = requests[i];
+        function doDelete() {
+          for (let i = 0; i < requests.length; i++) {
+            const file = requests[i];
 
-          if (file.done === null) {
-            file.done = false;
-            deleteOutput.style.display = '';
-            refresh.disabled = true;
+            if (file.type === 'delete' && file.done === null) {
+              file.done = false;
+              deleteContainer.style.display = '';
+              refreshContainer.style.display = '';
+              refresh.disabled = true;
 
-            const element = document.createElement('div');
-            element.style.marginBottom = '0.5em';
+              const element = document.createElement('div');
+              element.style.marginBottom = '0.5em';
 
-            const progress = document.createElement('div');
-            progress.style.display = 'inline-block';
-            progress.innerText = 'Working...';
-            progress.style.border = '1px solid #000';
-            progress.style.width = '100px';
-            progress.style.marginRight = '0.5em';
-            progress.style.textAlign = 'center';
-            element.appendChild(progress);
+              const progress = document.createElement('div');
+              progress.style.display = 'inline-block';
+              progress.innerText = 'Working...';
+              progress.style.border = '1px solid #000';
+              progress.style.width = '100px';
+              progress.style.marginRight = '0.5em';
+              progress.style.textAlign = 'center';
+              element.appendChild(progress);
 
-            const name = document.createElement('span');
-            name.innerText = file.name;
-            element.appendChild(name);
+              const name = document.createElement('span');
+              name.innerText = file.name;
+              element.appendChild(name);
 
-            deletes.appendChild(element);
+              deletes.appendChild(element);
 
-            const xhr = new XMLHttpRequest();
-            xhr.addEventListener('loadend', () => {
-              if (
-                xhr.readyState === 4 &&
-                xhr.status >= 200 &&
-                xhr.status < 300
-              ) {
-                progress.innerText = 'Success.';
-              } else {
-                progress.innerText = `Error: ${xhr.status} ${xhr.statusText}`;
-              }
-              file.done = true;
+              const xhr = new XMLHttpRequest();
+              xhr.addEventListener('loadend', () => {
+                if (
+                  xhr.readyState === 4 &&
+                  xhr.status >= 200 &&
+                  xhr.status < 300
+                ) {
+                  progress.innerText = 'Success.';
+                } else {
+                  progress.innerText = `Error: ${xhr.status} ${xhr.statusText}`;
+                }
+                file.done = true;
 
-              if (requests.find((file) => !file.done) == null) {
-                refresh.disabled = false;
-              }
-            });
-            xhr.open('DELETE', file.name, true);
-            xhr.send();
+                if (requests.find((request) => !request.done) == null) {
+                  refresh.disabled = false;
+                }
+              });
+              xhr.open('DELETE', file.name, true);
+              xhr.send();
+            }
           }
         }
       }
@@ -392,11 +394,7 @@
     <button>Submit</button>
   </form>
 
-  <div id="mkdirOutput" style="margin-top: 1em; display: none;">
-    <div id="mkdirs" />
-
-    <button id="refresh" onclick="window.location.reload()">Refresh</button>
-  </div>
+  <div id="mkdirs" style="margin-top: 1em; display: none;" />
 </div>
 
 <div id="uploadContainer" style="display: none;">
@@ -405,22 +403,20 @@
     <button>Submit</button>
   </form>
 
-  <div id="uploadOutput" style="margin-top: 1em; display: none;">
-    <div id="uploads" />
-
-    <button id="refresh" onclick="window.location.reload()">Refresh</button>
-  </div>
+  <div id="uploads" style="margin-top: 1em; display: none;" />
 </div>
 
-<div id="deleteOutput" style="margin-top: 1em; display: none;">
+<div id="deleteContainer" style="margin-top: 1em; display: none;">
   Delete Requests
 
   <div id="deletes" />
+</div>
 
+<div id="refreshContainer" style="margin-top: 1em; display: none;">
   <button id="refresh" onclick="window.location.reload()">Refresh</button>
 </div>
 
-<p>Powered by {name}</p>
+<p style="font-size: smaller;">Powered by {name}</p>
 
 <script>
   export let entries = [];
