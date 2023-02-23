0986c7223a3591e69454e335c0f7b22ebcd84405
Hunter Perrin <hperrin@gmail.com>
2023-02-23 09:38:55 -0800

    fix: nephele-serve script runs in macOS

--- packages/nephele-serve/nephele-serve.cjs
+++ packages/nephele-serve/nephele-serve.cjs
@@ -1,3 +1,3 @@
-#!/usr/bin/env -S NODE_OPTIONS="--experimental-specifier-resolution=node ${NODE_OPTIONS}" node
+#!/usr/bin/env -S NODE_OPTIONS="${NODE_OPTIONS}\_--experimental-specifier-resolution=node" node
 
 import('./bin/index.js');
